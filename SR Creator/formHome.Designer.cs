b4b7716057aafb863afcf992cb677e2a4a69ef8f
Maverick Peters <petersm6537@my.uwstout.edu>
2022-06-22 14:13:55 -0500

    Created home form

---
+++ SR Creator/formHome.Designer.cs
@@ -0,0 +1,160 @@
+﻿
+namespace SR_Creator
+{
+    partial class formHome
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            this.inputNew = new System.Windows.Forms.Button();
+            this.inputImport = new System.Windows.Forms.Button();
+            this.inputUpdate = new System.Windows.Forms.Button();
+            this.inputSettings = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(334, 311);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 1;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel2.Controls.Add(this.inputNew, 0, 0);
+            this.tableLayoutPanel2.Controls.Add(this.inputImport, 0, 1);
+            this.tableLayoutPanel2.Controls.Add(this.inputUpdate, 0, 2);
+            this.tableLayoutPanel2.Controls.Add(this.inputSettings, 0, 3);
+            this.tableLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(170, 3);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 4;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(161, 305);
+            this.tableLayoutPanel2.TabIndex = 0;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 2;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(161, 305);
+            this.tableLayoutPanel3.TabIndex = 1;
+            // 
+            // inputNew
+            // 
+            this.inputNew.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputNew.Location = new System.Drawing.Point(3, 3);
+            this.inputNew.Name = "inputNew";
+            this.inputNew.Size = new System.Drawing.Size(155, 70);
+            this.inputNew.TabIndex = 0;
+            this.inputNew.Text = "New";
+            this.inputNew.UseVisualStyleBackColor = true;
+            // 
+            // inputImport
+            // 
+            this.inputImport.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputImport.Location = new System.Drawing.Point(3, 79);
+            this.inputImport.Name = "inputImport";
+            this.inputImport.Size = new System.Drawing.Size(155, 70);
+            this.inputImport.TabIndex = 1;
+            this.inputImport.Text = "Import";
+            this.inputImport.UseVisualStyleBackColor = true;
+            // 
+            // inputUpdate
+            // 
+            this.inputUpdate.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputUpdate.Location = new System.Drawing.Point(3, 155);
+            this.inputUpdate.Name = "inputUpdate";
+            this.inputUpdate.Size = new System.Drawing.Size(155, 70);
+            this.inputUpdate.TabIndex = 2;
+            this.inputUpdate.Text = "Update SW File";
+            this.inputUpdate.UseVisualStyleBackColor = true;
+            // 
+            // inputSettings
+            // 
+            this.inputSettings.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.inputSettings.Location = new System.Drawing.Point(3, 231);
+            this.inputSettings.Name = "inputSettings";
+            this.inputSettings.Size = new System.Drawing.Size(155, 71);
+            this.inputSettings.TabIndex = 3;
+            this.inputSettings.Text = "Settings";
+            this.inputSettings.UseVisualStyleBackColor = true;
+            // 
+            // formHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 311);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.MaximumSize = new System.Drawing.Size(400, 400);
+            this.MinimumSize = new System.Drawing.Size(350, 350);
+            this.Name = "formHome";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SR Creator - Home";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.Button inputNew;
+        private System.Windows.Forms.Button inputImport;
+        private System.Windows.Forms.Button inputUpdate;
+        private System.Windows.Forms.Button inputSettings;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+    }
+}
+
